5c3170e86ba1f73ddfc7b6f9b63710172cd65f90
M.Ram charan <mummadicharan27@gmail.com>
2026-03-11 21:18:58 +0530

    fixed mobile hero buttons

--- script.js
+++ script.js
@@ -1,35 +1,64 @@
-new Typed("#typing",{
-strings:[
-"AI Engineer",
-"Machine Learning Developer",
-"Deep Learning Enthusiast"
-],
-typeSpeed:60,
-backSpeed:40,
-loop:true
+// Typing animation
+
+new Typed("#typing", {
+    strings: [
+        "AI Engineer",
+        "Machine Learning Developer",
+        "Deep Learning Enthusiast"
+    ],
+    typeSpeed: 60,
+    backSpeed: 40,
+    loop: true
 });
 
 
-const toggle=document.getElementById("themeToggle");
+// THEME TOGGLE
+
+const toggle = document.getElementById("themeToggle");
+
+toggle.onclick = () => {
+
+    document.body.classList.toggle("light");
+
+    if (document.body.classList.contains("light")) {
+        toggle.innerHTML = "🌙";
+    } else {
+        toggle.innerHTML = "☀️";
+    }
+};
+
+
+// MOBILE MENU
+
 const menuToggle = document.getElementById("menuToggle");
 const navMenu = document.getElementById("navMenu");
 
 menuToggle.onclick = () => {
     navMenu.classList.toggle("active");
 };
-toggle.onclick=()=>{
-document.body.classList.toggle("light");
-
-if(document.body.classList.contains("light")){
-toggle.innerHTML="🌙";
-}
-else{
-toggle.innerHTML="☀️";
-}
-};
 
 
-ScrollReveal().reveal('.hero',{delay:200});
-ScrollReveal().reveal('.project-card',{interval:200});
-ScrollReveal().reveal('.skills-grid span',{interval:100});
-ScrollReveal().reveal('.info-card',{interval:150});
+// CLOSE MENU AFTER CLICKING LINK (mobile)
+
+document.querySelectorAll(".nav a").forEach(link => {
+    link.addEventListener("click", () => {
+        navMenu.classList.remove("active");
+    });
+});
+
+
+// SCROLL REVEAL ANIMATIONS
+
+ScrollReveal().reveal('.hero', { delay: 200 });
+
+ScrollReveal().reveal('.project-card', {
+    interval: 200
+});
+
+ScrollReveal().reveal('.skills-grid span', {
+    interval: 100
+});
+
+ScrollReveal().reveal('.info-card', {
+    interval: 150
+});
